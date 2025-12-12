0eb87725f3c037550440d51c082ce9ed8aaf9efd
Laxman Kale <lakhankale888@gmail.com>
2025-12-12 21:44:33 +0530

    initialize project directory structure for backend

---
+++ backend/src/test/java/com/lucky/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucky.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
